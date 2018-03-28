fb3633eb4925a3e56728911371e1458a2462d50d
VagnerDanielSantos <michael.kiskao@gmail.com>
2018-03-28 16:51:53 -0300

    Este é o primeiro Commit deste arquivo em Machine Learning

---
+++ Resultados Credit Data.py
@@ -0,0 +1,11 @@
+
+CREDIT DATA
+
+0.9839 .... Árvore de decisão (Inconsistentes + Faltantes + Escalonamento)
+0.9379 .... Naive Bayes (Inconsistentes + Faltantes + Escalonamento)
+0.9839 .... Random Forest n_estimators = 40 (Inconsistentes + Faltantes + Escalonamento)
+0.9520 .... CN2 Learner (Sem Pré-Processamento)    
+0.8720 .... Base Line Classifier
+0.9860 .... KNN (Inconsistentes + Faltantes + Escalonamento)
+0.9440 .... Regressão Logística (Inconsistentes + Faltantes + Escalonamento)
+
