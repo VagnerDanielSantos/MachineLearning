2003aaee89abd6bc4ca47ba1359dcec2bf7bd9a7
VagnerDanielSantos <michael.kiskao@gmail.com>
2018-03-28 16:36:06 -0300

    Este é o primeiro Commit deste arquivo em Machine Learning

---
+++ arvore_decisao_risco_credito.py
@@ -0,0 +1,36 @@
+
+import pandas
+
+base = pandas.read_csv('E:\\Udemy - Cursos\\MachineLearning\\Arquivos\\RiscoCredito.csv')
+
+previsores = base.iloc[:, 0:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+previsores[:, 0] = labelencoder.fit_transform(previsores[:, 0])
+previsores[:, 1] = labelencoder.fit_transform(previsores[:, 1])
+previsores[:, 2] = labelencoder.fit_transform(previsores[:, 2])
+previsores[:, 3] = labelencoder.fit_transform(previsores[:, 3])
+
+from sklearn.tree import DecisionTreeClassifier, export
+
+classificador = DecisionTreeClassifier(criterion='entropy')
+classificador.fit(previsores, classe) # Constroi a árvore de decisão
+print(classificador.feature_importances_) # Mostra a importância de cada atributo
+
+export.export_graphviz( 
+                        classificador,
+                        out_file = 'arvore.dot',
+                        feature_names = ['Historia', 'Divida', 'Garantias', 'Renda'],
+                        class_names = ['Alto', 'Moderado', 'Baixo'],
+                        filled = True,
+                        leaves_parallel = True
+                      )
+# Histórico boa, divida alta, garantia nenhuma, renda > 35
+# Histórico ruim, dívida alta, garantia adequada, renda < 15
+
+resultado = classificador.predict([ [0, 0, 1, 2], [3, 0, 0, 0] ])
+print(classificador.classes_)
+print(classificador.class_count_)
+print(classificador.class_prior_)
