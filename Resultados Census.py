80566d83653681990820e3dad0238e34a39344c7
VagnerDanielSantos <michael.kiskao@gmail.com>
2018-03-28 16:51:01 -0300

    Este é o primeiro Commit deste arquivo em Machine Learning

---
+++ Resultados Census.py
@@ -0,0 +1,34 @@
+
+CENSUS
+
+0.4767 .... Nayve Bayes ( LabelEncoder + OneHotEncoder + Escalonamento)
+0.7952 .... Nayve Bayes ( LabelEncoder)
+0.7950 .... Nayve Bayes ( LabelEncoder + OneHotEncoder)
+0.8057 .... Nayve Bayes ( LabelEncoder + Escalonamento)
+ 
+ 
+0.8102 .... Árvore de Decisão ( LabelEncoder + OneHotEncoder + Escalonamento)
+0.8128 .... Árvore de Decisão ( LabelEncoder)
+0.8102 .... Árvore de Decisão ( LabelEncoder + OneHotEncoder)
+0.8128 .... Árvore de Decisão ( LabelEncoder + Escalonamento)
+
+0.8393 .... Random Forest ( LabelEncoder + OneHotEncoder + Escalonamento + n_estimators = 10)
+0.8401 .... Random Forest ( LabelEncoder + OneHotEncoder + Escalonamento + n_estimators = 15)
+0.8479 .... Random Forest ( LabelEncoder + OneHotEncoder + Escalonamento + n_estimators = 40)
+0.8481 .... Random Forest ( LabelEncoder + n_estimators = 40)
+0.8489 .... Random Forest ( LabelEncoder + OneHotEncoder + n_estimators = 40)
+0.8479 .... Random Forest ( LabelEncoder + Escalonamento + n_estimators = 40)
+
+0.8090 .... CN2 Learner (Sem Pré-Processamento)
+
+0.7759 .... Base Line Classifier (ZeroR = zero regra)
+
+0.8223 .... KNN ( LabelEncoder + OneHotEncoder + Escalonamento)
+0.7746 .... KNN ( LabelEncoder)
+0.7760 .... KNN ( LabelEncoder + OneHotEncoder)
+0.8219 .... KNN ( LabelEncoder + Escalonamento)
+
+0.8495 .... Regressão Logística ( LabelEncoder + OneHotEncoder + Escalonamento)
+0.7909 .... Regressão Logística ( LabelEncoder)
+0.7954 .... Regressão Logística ( LabelEncoder + OneHotEncoder)
+0.8184 .... Regressão Logística ( LabelEncoder + Escalonamento)
